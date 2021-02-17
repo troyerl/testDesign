a4ee662a7535dea846e77457d634758c6b1577dc
Logan Troyer <troyerlogan@gmail.com>
2021-02-17 09:10:22 -0500

    Init commit

---
+++ src/layout/DashboardLayout.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="wrapper">
+    <side-bar>
+      <sidebar-link to="/admin/overview">
+        <i class="nc-icon nc-chart-pie-35"></i>
+        <p>Dashboard</p>
+      </sidebar-link>
+      <sidebar-link to="/admin/user">
+        <i class="nc-icon nc-circle-09"></i>
+        <p>User Profile</p>
+      </sidebar-link>
+      <sidebar-link to="/admin/table-list">
+        <i class="nc-icon nc-notes"></i>
+        <p>Playlists</p>
+      </sidebar-link>
+      <sidebar-link to="/admin/typography">
+        <i class="nc-icon nc-paper-2"></i>
+        <p>New Playlist</p>
+      </sidebar-link>
+      <sidebar-link to="/admin/maps">
+        <i class="nc-icon nc-pin-3"></i>
+        <p>Maps</p>
+      </sidebar-link>
+
+      <template slot="bottom-links">
+        <sidebar-link class="active"
+                      to="/admin/upgrade">
+          <i class="nc-icon nc-alien-33"></i>
+          <p>Log out</p>
+        </sidebar-link>
+      </template>
+    </side-bar>
+    <div class="main-panel">
+      <top-navbar></top-navbar>
+
+      <dashboard-content @click="toggleSidebar">
+
+      </dashboard-content>
+
+      <content-footer></content-footer>
+    </div>
+  </div>
+</template>
+<style lang="scss">
+
+</style>
+<script>
+  import TopNavbar from './TopNavbar.vue'
+  import ContentFooter from './ContentFooter.vue'
+  import DashboardContent from './Content.vue'
+  export default {
+    components: {
+      TopNavbar,
+      ContentFooter,
+      DashboardContent,
+    },
+    methods: {
+      toggleSidebar () {
+        if (this.$sidebar.showSidebar) {
+          this.$sidebar.displaySidebar(false)
+        }
+      }
+    }
+  }
+
+</script>
---
+++ src/layout/MobileMenu.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul class="nav nav-mobile-menu">
+    <li class="nav-item">
+      <a class="nav-link" href="#pablo">
+        <span class="no-icon">Log out</span>
+      </a>
+    </li>
+  </ul>
+</template>
+<script>
+  export default {
+    name: 'mobile-menu'
+  }
+</script>
+<style>
+</style>
